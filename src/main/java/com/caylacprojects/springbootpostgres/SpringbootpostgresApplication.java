131f8f8b841b8485505f2e2d432ab494637c24d6
Cayla <cay.curry01@gmail.com>
2024-10-31 14:14:28 -0400

    create .env file for database security

--- src/main/java/com/caylacprojects/springbootpostgres/SpringbootpostgresApplication.java
+++ src/main/java/com/caylacprojects/springbootpostgres/SpringbootpostgresApplication.java
@@ -4,11 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.caylacprojects") // Adjust if necessary to match your exact package structure
 public class SpringbootpostgresApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// test print
+		// System.out.println("Database URL: " + dotenv.get("DATABASE_URL"));
+    	// System.out.println("Database Username: " + dotenv.get("DATABASE_USERNAME"));
+		// System.out.println("Database Username: " + dotenv.get("DATABASE_PASSWORD"));
+
+		// Set environment variables explicitly if needed (optional step)
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(SpringbootpostgresApplication.class, args);
 	}
 }
